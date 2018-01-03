0cb0748407b4994fb5a337f9912fc8d43fe0ba3a
Daniel Dunbar <danielqdunbar@gmail.com>
2018-01-03 01:07:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,10 @@
 var forLoop = (array) => {
   for (let i = 0; i < 25; i++){
     if (i===0) {
-      return array.push('I am 1 strange loop.')
+      array.push('I am 1 strange loop.')
     } else {
-      return array.push(`I am ${i} strange loops.`)
+      array.push(`I am ${i} strange loops.`)
     }
   }
+  return array
 }
